75af2ed890ddbb9673e91e578197f8364d5c6bb1
krishjem <10qg5423LD>
2022-03-09 12:49:41 +0100

    project setup

---
+++ android/src/com/mygdx/gardenguard/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.gardenguard;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.gardenguard.GardenGuard;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GardenGuard(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/gardenguard/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gardenguard.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gardenguard.GardenGuard;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GardenGuard(), config);
+	}
+}
