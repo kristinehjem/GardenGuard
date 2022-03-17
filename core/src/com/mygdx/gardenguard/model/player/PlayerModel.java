75162b8fa5d4498ee79ae9bebe6b95e60fa7482d
sondrewl <sondrewl@stud.ntnu.no>
2022-03-17 08:52:01 +0100

    Made different playermodels

--- core/src/com/mygdx/gardenguard/model/player/PlayerModel.java
+++ core/src/com/mygdx/gardenguard/model/player/PlayerModel.java
@@ -1,29 +1,57 @@
 package com.mygdx.gardenguard.model.player;
 
-import com.badlogic.gdx.math.Vector3;
-
-public class PlayerModel {
+public abstract class PlayerModel {
 
     private String playerID;
-    private String role;      // Lage roller seperat fra player modellen?
-    private Vector3 position;
-    private boolean solid;
+    private String position; //Skal ha et board (Board) som består av tiles (Tile).
     private int score;
+    private boolean max_vision;
+    private int steps;
     //private firebase for å kunne lage playerID og lagre informasjonen
     //private int face: Sier noe om hvilken retning spriten ser.
 
-
-    public PlayerModel(String role, String playerID, Vector3 position){
+    public PlayerModel(String playerID, String postion){
 
         this.playerID = playerID;
-        this.solid = false;
         this.score = 0;
         this.position = position;
-
-
     }
 
+    protected void setSteps(int i) {
+        this.steps = i;
+    }
 
+    public String getPosition() {
+        return position;
+    }
 
+    public boolean isMax_vision() {
+        return max_vision;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public String getPlayerID() {
+        return playerID;
+    }
+
+    protected void setMax_vision(boolean max_vision) {
+        this.max_vision = max_vision;
+    }
+
+    public void setPlayerID(String playerID) {
+        this.playerID = playerID;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 
 }
+
