9215dd033d8c07bf3d41051e467096e3e191ebaa
kristinehjem <kristinehjemgard@hotmail.com>
2022-03-24 17:52:04 +0100

    #12 added buttons and a pop up for pin input

--- core/src/com/mygdx/gardenguard/view/MenuState.java
+++ core/src/com/mygdx/gardenguard/view/MenuState.java
@@ -1,26 +1,31 @@
 package com.mygdx.gardenguard.view;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.Input.TextInputListener;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.gardenguard.GardenGuard;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 import java.awt.Color;
 
-public class MenuState extends State{
+public class MenuState extends State implements TextInputListener {
 
     BitmapFont name, font;
     Texture bg = new Texture("newBg.jpg");
 
-    Stage stage;
-
+    private Stage stage;
+    public String pin;
 
     public MenuState(){
         super();
@@ -40,9 +45,10 @@ public class MenuState extends State{
     @Override
     protected void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
+        //sb.draw(bg, 0,0, GardenGuard.WIDTH, GardenGuard.HEIGHT);
+        Gdx.gl.glClearColor(0,0,0,0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         create();
-        sb.draw(bg, 0,0, GardenGuard.WIDTH, GardenGuard.HEIGHT);
-        //name.draw(sb, "Garden Guard", 160, 150);
         stage.act();
         stage.draw();
     }
@@ -57,5 +63,46 @@ public class MenuState extends State{
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
         Skin mySkin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+        Button join = new TextButton("Join game", mySkin, "small");
+        Button create = new TextButton("Create game", mySkin, "small");
+        join.setSize(100, 50);
+        join.setPosition(170, 400);
+        join.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("join");
+                Gdx.input.getTextInput(getThis(), "Enter game pin", "", "");
+                //pin attribute will now be updated with the typed pin
+                //if for å sjekke om pin er gyldig
+                //GameStateManager.getInstance().push(new LobbyState());
+                return true;
+            }
+        });
+        create.setSize(130, 50);
+        create.setPosition(170, 330);
+        create.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                //GameStateManager.getInstance().push(new LobbyState());
+                System.out.println("create");
+                return true;
+            }
+        });
+        stage.addActor(join);
+        stage.addActor(create);
     }
+
+    @Override
+    public void input(String text) {
+        this.pin = text;
+    }
+
+    @Override
+    public void canceled() {
+        pin = "cancelled";
+    }
+
+    public TextInputListener getThis() {
+        return this;
+    };
 }
