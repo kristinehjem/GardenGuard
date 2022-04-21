72b14865c4e573572753f82878849adf91ea6579
Goshton <swliestol@gmail.com>
2022-04-21 14:56:53 +0200

    Edited db call for score

--- core/src/com/mygdx/gardenguard/controller/stateControllers/PlayStateController.java
+++ core/src/com/mygdx/gardenguard/controller/stateControllers/PlayStateController.java
@@ -98,10 +98,10 @@ public class PlayStateController extends Controller {
         //playerController.getPlayer().setIsFound(gsm.getPlayer().getIsFound());
         System.out.println("GIR_POENG: "+ super.gsm.getPlayers());
         for(PlayerModel player : super.getPlayers()) {
-            if(player.getPlayerID().equals(super.gsm.getPlayer().getPlayerID()) && getPlayer() instanceof HiderModel && !player.getIsFound()) {
+            if(player.getPlayerID().equals(super.gsm.getPlayer().getPlayerID())
+                    && getPlayer() instanceof HiderModel && !player.getIsFound()) {
                 getPlayer().setScore(getPlayer().getScore() + 20);
                 super.gsm.getFBIC().UpdateScoreInDB(gsm.getGamePin(), gsm.getPlayer().getPlayerID(), getPlayer().getScore());
-                super.gsm.getFBIC().UpdateIsFoundInDB(gsm.getGamePin(), gsm.getPlayer().getPlayerID(), false);
             }
         }
         // player gets 20 points if it is not found
@@ -118,6 +118,7 @@ public class PlayStateController extends Controller {
         increaseRounds();
         resetSteps(gsm.getPlayer());
         isSeekerTurn = false;
+        super.gsm.getFBIC().UpdateIsFoundInDB(gsm.getGamePin(), gsm.getPlayer().getPlayerID(), false);
     }
 
     public void increaseRounds() {
